3eb7d99034630751ebc1c3e9c44e2e98b81b89cb
liubin <liumeile0608@163.com>
2018-06-08 17:34:26 +0800

    323

--- app01/views.py
+++ app01/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from .models import *
+import json
 # Create your views here.
 
 
 def students(request):
 
+    cls_list = Classes.objects.all()
+
     stu_list = Student.objects.all()
 
-    return render(request,'students.html',{'stu_list':stu_list})
 
+
+    return render(request,'students.html',{'cls_list':cls_list,'stu_list':stu_list})
+
+
+def add_student(request):
+    """
+    添加学生
+    :param request: 
+    :return: 
+    """
+
+    response = {'status':True,'message':None}
+
+    try:
+
+        u = request.POST.get('username')
+        a = request.POST.get('age')
+        g = request.POST.get('gender')
+        c = request.POST.get('cls_id')
+
+        Student.objects.create(
+            username=u,
+            age=a,
+            gender=g,
+            cs_id=c)
+
+    except Exception as e:
+
+        response['status'] = False
+        response['message'] = "用户输入错误！"
+
+
+
+    result = json.dumps(response)
+    return HttpResponse(result)
 
 
--- day56/urls.py
+++ day56/urls.py
@@ -18,5 +18,8 @@ from django.contrib import admin
 from app01 import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^students/',views.students)
+    url(r'^students/',views.students),
+    url(r'^add_student/',views.add_student),
+
+
 ]
